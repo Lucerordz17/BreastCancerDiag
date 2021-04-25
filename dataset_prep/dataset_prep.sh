fb9c106de617514b0bc2ae0aa189632539cc20e0
Lucerordz17 <83086578+Lucerordz17@users.noreply.github.com>
2021-04-25 13:04:35 -0700

    Update dataset_prep.sh

--- dataset_prep/dataset_prep.sh
+++ dataset_prep/dataset_prep.sh
@@ -3,6 +3,11 @@ pip -q uninstall -y kaggle
 pip -q install --upgrade pip
 pip3 -q install --upgrade kaggle
 
+pip -q install pydicom
+pip -q install opencv-python
+pip -q install pillow # optional
+pip -q install pandas
+
 mkdir /root/.kaggle
 echo '{"username":"lucerorodriguez","key":"f9960528042fead1f6039c3a876df225"}' > /root/.kaggle/kaggle.json
 

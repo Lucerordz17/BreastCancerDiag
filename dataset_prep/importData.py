c8f5b18b47752207cc8fff3dee9fd7dc2921f7ef
Lucerordz17 <83086578+Lucerordz17@users.noreply.github.com>
2021-04-25 19:20:14 -0700

    Add files via upload

--- dataset_prep/importData.py
+++ dataset_prep/importData.py
@@ -1,6 +1,6 @@
 import os
 import cv2
-#import PIL # optional
+import PIL # optional
 import csv
 import pandas as pd
 import numpy as np
@@ -8,19 +8,19 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 GooglePath = "/content/gdrive/MyDrive/Breast Cancer Data"
-savepath = 'data'
+savepath = '/content/data'
 images_path1 = "/content/gdrive/MyDrive/Breast Cancer Data/train"
 images_path2 = "/content/gdrive/MyDrive/Breast Cancer Data/test"
 
 path1 = os.listdir(images_path1)
 for image in enumerate(path1):
-    image_cv2 = cv2.imread(os.path.join(images_path1, image))
-    cv2.imwrite(os.path.join(savepath, 'train', image), image_cv2)
+    image_cv2 = Image.open(os.path.join(images_path1, image))
+    cv2.save(os.path.join(savepath, 'train', image))
 
 path2 = os.listdir(images_path2)
 for image in enumerate(path2):
-    image_cv2 = cv2.imread(os.path.join(images_path2, image))
-    cv2.imwrite(os.path.join(savepath, 'test', image), image_cv2)
+    image_cv2 = Image.open(os.path.join(images_path2, image))
+    cv2.save(os.path.join(savepath, 'test', image))
 
 f1 = open(GooglePath+"test_split_v3.txt", "r")
 contents1 = f1.open()

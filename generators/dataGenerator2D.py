27335b36b2e4d768a31f742b813e390b61f5cb29
Lucerordz17 <83086578+Lucerordz17@users.noreply.github.com>
2021-04-26 20:52:39 -0700

    Add files via upload

--- generators/dataGenerator2D.py
+++ generators/dataGenerator2D.py
@@ -5,9 +5,11 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow import keras
 
+
 def crop_top(img, percent=0.15):
     offset = int(img.shape[0] * percent)
     return img[offset:]
+
 
 def central_crop(img):
     size = min(img.shape[0], img.shape[1])
@@ -15,19 +17,22 @@ def central_crop(img):
     offset_w = int((img.shape[1] - size) / 2)
     return img[offset_h:offset_h + size, offset_w:offset_w + size]
 
+
 def process_image_file(filepath, top_percent, size, enhance=False):
     img = cv2.imread(filepath)
     img = crop_top(img, percent=top_percent)
     img = central_crop(img)
     img = cv2.resize(img, (size, size))
     if enhance:
-      img = xray_enhance(img)
+        img = xray_enhance(img)
     return img
 
+
 def xray_enhance(img):
-  gaussian_3 = cv2.GaussianBlur(img, (3,3), 10.0)
-  unsharp_image = cv2.addWeighted(img, 1.5, gaussian_3, -0.5, 0, img)
-  return unsharp_image
+    gaussian_3 = cv2.GaussianBlur(img, (3, 3), 10.0)
+    unsharp_image = cv2.addWeighted(img, 1.5, gaussian_3, -0.5, 0, img)
+    return unsharp_image
+
 
 _augmentation_transform = ImageDataGenerator(
     featurewise_center=False,
@@ -46,8 +51,7 @@ def apply_augmentation(img):
     img = _augmentation_transform.random_transform(img)
     return img
 
-
-class BalanceCovidDataset(tf.keras.utils.Sequence):
+class BalanceMalDataset(tf.keras.utils.Sequence):
     'Generates data for Keras'
 
     def __init__(
@@ -56,16 +60,15 @@ class BalanceCovidDataset(tf.keras.utils.Sequence):
             is_training=True,
             batch_size=8,
             input_shape=(224, 224),
-            n_classes=3,
+            n_classes=2,
             num_channels=3,
             mapping={
-                'normal': 0,
-                'pneumonia': 1,
-                'COVID-19': 2
+                'BENIGN': 0,
+                'MALIGNANT': 1
             },
             shuffle=True,
             augmentation=apply_augmentation,
-            covid_percent=0.3,
+            mal_percent=0.3,
             top_percent=0.08,
             balancing=False,
             enhance=False,
@@ -81,7 +84,7 @@ class BalanceCovidDataset(tf.keras.utils.Sequence):
         self.num_channels = num_channels
         self.mapping = mapping
         self.shuffle = True
-        self.covid_percent = covid_percent
+        self.mal_percent = mal_percent
         self.n = 0
         self.augmentation = augmentation
         self.top_percent = top_percent
@@ -89,21 +92,21 @@ class BalanceCovidDataset(tf.keras.utils.Sequence):
         self.mean = None
         self.std = None
         self.enhance = enhance
-        self.aug=aug
+        self.aug = aug
 
-        datasets = {'normal': [], 'pneumonia': [], 'COVID-19': []}
+        datasets = {'BENIGN': [], 'MALIGNANT': []}
         for l in self.dataset:
-          datasets[l[2]].append(l)
+            datasets[l[2]].append(l)
         if self.balancing:
-          self.datasets = [
-              datasets['normal'] + datasets['pneumonia'],
-              datasets['COVID-19'],
-          ]
+            self.datasets = [
+                datasets['BENIGN'],
+                datasets['MALIGNANT'],
+            ]
         else:
-          self.datasets = [
-              datasets['normal'] + datasets['pneumonia'] + datasets['COVID-19']
-          ]
-          #print(len(self.datasets[0]), len(self.datasets[1]))
+            self.datasets = [
+                datasets['BENIGN'] + datasets['MALIGNANT']
+            ]
+            # print(len(self.datasets[0]), len(self.datasets[1]))
 
         self.on_epoch_end()
 
@@ -130,8 +133,8 @@ class BalanceCovidDataset(tf.keras.utils.Sequence):
                 np.random.shuffle(v)
 
     def include_statistics(mean, std):
-      self.mean = mean
-      self.std = std
+        self.mean = mean
+        self.std = std
 
     def __getitem__(self, idx):
         batch_x, batch_y = np.zeros(
@@ -139,24 +142,23 @@ class BalanceCovidDataset(tf.keras.utils.Sequence):
              self.num_channels)), np.zeros(self.batch_size)
 
         batch_files = self.datasets[0][idx * self.batch_size:(idx + 1) *
-                                       self.batch_size]
+                                                             self.batch_size]
         if self.balancing:
-       # upsample covid cases
-          covid_size = max(int(len(batch_files) * self.covid_percent), 1)
-          covid_inds = np.random.choice(np.arange(len(batch_files)),
-                                        size=covid_size,
-                                        replace=False)
-         
-          choices_indices = np.random.choice(np.arange(len(self.datasets[1])),
-                                        size=covid_size,
-                                        replace=False)
-          covid_files = [self.datasets[1][i] for i in choices_indices]
-          for i in range(covid_size):
-              batch_files[covid_inds[i]] = covid_files[i]
+            # upsample malignant cases
+            mal_size = max(int(len(batch_files) * self.mal_percent), 1)
+            mal_inds = np.random.choice(np.arange(len(batch_files)),
+                                          size=mal_size,
+                                          replace=False)
+
+            choices_indices = np.random.choice(np.arange(len(self.datasets[1])),
+                                               size=mal_size,
+                                               replace=False)
+            mal_files = [self.datasets[1][i] for i in choices_indices]
+            for i in range(mal_size):
+                batch_files[mal_inds[i]] = mal_files[i]
 
         for i in range(len(batch_files)):
             sample = batch_files[i]
-
 
             x = process_image_file(sample[1],
                                    self.top_percent,
@@ -167,7 +169,7 @@ class BalanceCovidDataset(tf.keras.utils.Sequence):
 
             x = x.astype('float32') / 255.0
             if self.mean is not None and self.std is not None:
-              x = (x-mean)/std
+                x = (x - mean) / std
 
             y = self.mapping[sample[2]]
 
